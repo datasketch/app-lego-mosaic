662098b16e5385efdf83b72aa12e30130dfc6f72
brazadas <anaherand@hotmail.com>
2020-07-31 13:48:18 -0500

    get link in dropdown (interface only); 'safely' generating the mosaic as gender-app

--- app.R
+++ app.R
@@ -4,13 +4,16 @@ library(shinyinvoer)
 library(shi18ny)
 library(V8)
 library(dsmodules)
-library(purrr)
 library(brickr)
+library(shinycustomloader)
+library(magick)
 
 # Parámetros width y height no son tan independientes
-# download initial image svg y pdf
+# loaders con el dropdown...
 
-ui <- panelsPage(useShi18ny(),
+ui <- panelsPage(includeScript(paste0(system.file("js/", package = "dsmodules"), "downloadGen.js")),
+                 useShi18ny(),
+                 showDebug(),
                  panel(title = ui_("upload_data"),
                        width = 200,
                        body = uiOutput("image_input")),
@@ -30,15 +33,11 @@ ui <- panelsPage(useShi18ny(),
                                               src = "data:image/gif;base64,R0lGODlhEAALAPQAAP///wAAANra2tDQ0Orq6gYGBgAAAC4uLoKCgmBgYLq6uiIiIkpKSoqKimRkZL6+viYmJgQEBE5OTubm5tjY2PT09Dg4ONzc3PLy8ra2tqCgoMrKyu7u7gAAAAAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCwAAACwAAAAAEAALAAAFLSAgjmRpnqSgCuLKAq5AEIM4zDVw03ve27ifDgfkEYe04kDIDC5zrtYKRa2WQgAh+QQJCwAAACwAAAAAEAALAAAFJGBhGAVgnqhpHIeRvsDawqns0qeN5+y967tYLyicBYE7EYkYAgAh+QQJCwAAACwAAAAAEAALAAAFNiAgjothLOOIJAkiGgxjpGKiKMkbz7SN6zIawJcDwIK9W/HISxGBzdHTuBNOmcJVCyoUlk7CEAAh+QQJCwAAACwAAAAAEAALAAAFNSAgjqQIRRFUAo3jNGIkSdHqPI8Tz3V55zuaDacDyIQ+YrBH+hWPzJFzOQQaeavWi7oqnVIhACH5BAkLAAAALAAAAAAQAAsAAAUyICCOZGme1rJY5kRRk7hI0mJSVUXJtF3iOl7tltsBZsNfUegjAY3I5sgFY55KqdX1GgIAIfkECQsAAAAsAAAAABAACwAABTcgII5kaZ4kcV2EqLJipmnZhWGXaOOitm2aXQ4g7P2Ct2ER4AMul00kj5g0Al8tADY2y6C+4FIIACH5BAkLAAAALAAAAAAQAAsAAAUvICCOZGme5ERRk6iy7qpyHCVStA3gNa/7txxwlwv2isSacYUc+l4tADQGQ1mvpBAAIfkECQsAAAAsAAAAABAACwAABS8gII5kaZ7kRFGTqLLuqnIcJVK0DeA1r/u3HHCXC/aKxJpxhRz6Xi0ANAZDWa+kEAA7AAAAAAAAAAAA"),
                                           HTML("<i class = 'btn-done-indicator fa fa-check' style = 'display: none; margin-left: 18px;'> </i>")))),
                  panel(title = ui_("viz"),
+                       title_plugin = uiOutput("download"),
                        color = "chardonnay",
                        can_collapse = FALSE,
                        body = div(langSelectorInput("lang", position = "fixed"),
-                                  uiOutput("result", style = "text-align: center;"),
-                                  shinypanels::modal(id = "download",
-                                                     title = ui_("download_image"),
-                                                     uiOutput("modal"))),
-                       footer = shinypanels::modalButton(label = "Download image", modal_id = "download")))
-
+                                  withLoader(uiOutput("result", style = "text-align: center;"), type = "image", loader = "loading_gris.gif"))))
 
 
 server <- function(input, output, session) {
@@ -116,6 +115,7 @@ server <- function(input, output, session) {
     r1 <- tryCatch(jpeg::readJPEG(lg), error = function(e) e)
     w0 <- which(c(!any(grepl("error", class(r0))), !any(grepl("error", class(r1))))) - 1
     r2 <- get(paste0("r", w0))
+    # r2 <- image_read(lg)
     plot_lego$img <- r2
   })
   
@@ -147,57 +147,55 @@ server <- function(input, output, session) {
     } else {
       floor((r0[1] * scl) / r0[2])
     }
-    # r0[2]
   })
   
   # gráfica de lego
   observeEvent(input$generate, {
-    lapply(c("jpeg", "png", "svg", "pdf"), function(z) {
-      buttonId <- paste0("download_data_button-DownloadImg", z)
-      session$sendCustomMessage("setButtonState", c("none", buttonId)) 
-    })
-    # if (is.null(plot_lego$img)) {
-    # if (input$`initial_data-inputDataSample` == "www/abs.jpg") {
-    #   # Sys.sleep(10)
-    #   plt <- jpeg::readJPEG("www/abs.jpg") %>%
-    #     image_to_mosaic(img_size = c(90, 60),
-    #                     # method = input$method,
-    #     ) %>% 
-    #     build_mosaic()
-    #   plot_lego$plt <- plt
-    #   print("W")
-    # } else {
-    
     session$sendCustomMessage("setButtonState", c("loading", "generate_bt"))
+    safe_image_to_mosaic <- purrr::safely(image_to_mosaic)
     plt <- plot_lego$img %>%
-      image_to_mosaic(img_size = c(input$width, input$height),
-                      # color_table = input$color_table_img,
-                      # dithering = input$dithering,
-                      method = input$method,
-                      color_palette = input$color_palette,
-                      contrast = input$contrast,
-                      brightness = input$brightness) %>% 
-      build_mosaic()
+      safe_image_to_mosaic(img_size = c(input$width, input$height),
+                           # color_table = input$color_table_img,
+                           # dithering = input$dithering,
+                           method = input$method,
+                           color_palette = input$color_palette,
+                           contrast = input$contrast,
+                           brightness = input$brightness)
+    
+    if (!is.null(plt$result)) plt$result <- build_mosaic(plt$result) 
     plot_lego$plt <- plt
     session$sendCustomMessage("setButtonState", c("done", "generate_bt"))
     # }
   })
   # }, ignoreNULL = FALSE)
   
+  output$download <- renderUI({
+    lb <- i_("download_image", lang())
+    dw <- i_("download", lang())
+    gl <- i_("get_link", lang())
+    downloadImageUI("download_data_button", dropdownLabel = lb, text = dw, formats = c("link", "jpeg", "png"),# "svg", "pdf"), 
+                    display = "dropdown", dropdownWidth = 170, getLinkLabel = gl, modalTitle = gl)
+  })
+  
   # renderizando mosaico ggplot
   output$result <- renderUI({
     ls <- list("www/abs.jpg" = "abs_lg.png",
                "www/book_rug.jpg" = "book_rug_lg.jpeg",
                "www/horse.png" = "horse_lg.jpeg")
-    if (input$`initial_data-imageInput` == "sampleData") {
+    if (input$`initial_data-imageInput` == "sampleData" && input$generate == 0) {
       img(src = ls[[plot_lego$dtin()$src]])
     } else {
-      plotOutput("plot", height = "61vh")
+      res <- plot_lego$plt
+      if (is.null(res$result)) {
+        infomessage(p(res$error$message))
+      } else {
+        plotOutput("plot", height = "61vh")
+      }
     }
   })
   
   output$plot <- renderPlot({
-    plot_lego$plt
+    plot_lego$plt$result
   })
   
   # quitando el chulo de generado cuando cualquier parámetro cambia
@@ -205,40 +203,39 @@ server <- function(input, output, session) {
     session$sendCustomMessage("setButtonState", c("none", "generate_bt"))
   })
   
-  output$modal <- renderUI({
-    dw <- i_("download", lang())
-    downloadImageUI("download_data_button", dw, formats = c("jpeg", "png", "svg", "pdf"))
-  })
-  
   # descargas
   observe({
     req(input$`initial_data-inputDataSample`)
+    # ls <- list("www/abs.jpg" = "abs_lg.svg")#,
     ls <- list("www/abs.jpg" = "abs_lg.jpeg",
                "www/book_rug.jpg" = "book_rug_lg.jpeg",
                "www/horse.png" = "horse_lg.jpeg")
     if (input$`initial_data-imageInput` == "sampleData" & input$generate == 0) {
+      # r0 <- image_read_svg(paste0("www/", ls[[plot_lego$dtin()$src]]))
       r0 <- jpeg::readJPEG(paste0("www/", ls[[plot_lego$dtin()$src]]))
-      lapply(1:4, function(z) {
+      lapply(1:2, function(z) {
         fn <- c("jpeg::writeJPEG", "png::writePNG", "rsvg::rsvg_svg", "rsvg::rsvg_pdf")[z]
         fr <- c("jpeg", "png", "svg", "pdf")[z]
         buttonId <- paste0("download_data_button-DownloadImg", fr)
-        session$sendCustomMessage("setButtonState", c("none", buttonId))
         output[[paste0("download_data_button-DownloadImg", fr)]] <- shiny::downloadHandler(filename = function() {
-          session$sendCustomMessage("setButtonState", c("loading", buttonId))
           paste0("plot-", gsub(" ", "_", substr(as.POSIXct(Sys.time()), 1, 19)), ".", fr)
         }, content = function(file) {
-          if (z %in% 3:4) {
-            tmp <- paste(tempdir(), "svg", sep = ".")
-            svglite::svglite(tmp, width = 10, height = 7)
-            plt0
-            dev.off()
-          }
+          # if (z %in% 3) {
+          #   r0 <- image_read_svg(paste0("www/", ls[[plot_lego$dtin()$src]]))
+          #   
+          #   # tmp <- paste(tempdir(), "svg", sep = ".")
+          #   # svglite::svglite(tmp, width = 10, height = 7)
+          #   # plt0
+          #   # dev.off()
+          # } else {
+          #   r0 <- image_read(paste0("www/", ls[[plot_lego$dtin()$src]]))
+          # }
+          # image_write(r0, file, format = fr)
           do.call(eval(parse(text = fn)), list(r0, file))
-          session$sendCustomMessage("setButtonState", c("done", buttonId))
         })
       })
     } else {
-      callModule(downloadImage, "download_data_button", graph = reactive(plot_lego$plt), lib = "ggplot", formats = c("jpeg", "png", "svg", "pdf"))
+      callModule(downloadImage, "download_data_button", graph = reactive(plot_lego$plt$result), lib = "ggplot", formats = c("link", "jpeg", "png"))#, "svg", "pdf"))
     }
   })
   
